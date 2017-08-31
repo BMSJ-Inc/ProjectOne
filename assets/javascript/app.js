bbb8edead879c705e870b9ea43513050eba8d17d
Michael Nehas <Michael Nehas>
2017-08-31 19:35:37 -0400

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
 // transit and trails API key: d9881e8b3dd8b87a86a70c42a084d3f4029e9934d3657a6c4c0e71f91f7f79c0
 
 // OpenWeatherMap API key: 9bd46c34cf611bf254ee8e90b766ad25
+
+// google places api key AIzaSyCs2eO_yLfEGdntBqOvVJMsZL5ONGtBwHM
