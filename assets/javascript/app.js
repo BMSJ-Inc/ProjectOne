ff5ec04ff3af19599dc430023d2538ca5ca79ef1
Brooke Warren <brookewarren@Brocs-MacBook-Pro.local>
2017-09-04 00:47:54 -0400

    weather api ajax call and on click function to submit button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,33 @@
 // transit and trails API key: d9881e8b3dd8b87a86a70c42a084d3f4029e9934d3657a6c4c0e71f91f7f79c0
 
-// OpenWeatherMap API key: 9bd46c34cf611bf254ee8e90b766ad25
-
 // google places api key AIzaSyCs2eO_yLfEGdntBqOvVJMsZL5ONGtBwHM
+
+$(document).ready(function(){
+
+
+	var APIKey = "d9881e8b3dd8b87a86a70c42a084d3f4029e9934d3657a6c4c0e71f91f7f79c0";
+  
+    var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?";
+   
+   var getAjax = function(url){
+
+    var apiUrl = url;   
+    $.ajax({
+        url: apiUrl,
+        method: "GET",
+        success: function(data) {
+        	console.log(data)
+         return data;
+      	}
+    });
+	};
+
+	$("#weatherSearch").on("click", function(){ 
+		var city = $("#city").val().trim();
+		getAjax("http://api.openweathermap.org" + "/data/2.5/weather" + "?q=" + city + "&appid=8f1e1fe8ad21fd905884311228d4b797")
+	})
+
+
+	
+
+});
