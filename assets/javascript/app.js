e0c3ded2ed2aa67baa6881576e493bcc43e18b60
Brooke Warren <brookewarren@Brocs-MacBook-Pro.local>
2017-09-07 01:30:10 -0400

    Added trails api, added google api, added user validation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,9 +3,13 @@
 // google places api key AIzaSyA1xuQdW9H5SBfN8XjZtAJKifKs30YBIqk
 //google geocoding api key: AIzaSyBOglb0LjVuwKiFDQz52dk_MSTfMJvbq5g
 //myTrails api key: q8z7f7e2x9n1z8k7b4h2
-
+//mapbox key: pk.eyJ1IjoiZHVja2llYmVld2FycmVuIiwiYSI6ImNqNzl1cXMwajA3aTcyd2xhazU0Y3cyd2UifQ.jR30zEe-DjeGZSakFXo4-g
 
 $(document).ready(function(){
+
+  //Global variables
+  var searchLocation;
+  var trailLocations = [];
 
   var getAjax = function(url){
 
@@ -14,6 +18,11 @@ $(document).ready(function(){
     $.ajax({
       url: url,
       method: "GET",
+      // headers: {
+      //   "X-Mashape-Key":"FtPQ6JmTVtmshoiz8rYTQyhg7M7zp1A2x1NjsnfqQ6kJxvwTXr",
+      //   "Accept":"text/plain"
+      // }
+      //Must put "async: false" property to return result from api. Why???
       async: false,
       success: function(data) {
         //console.log(data.coord.lon)
@@ -39,36 +48,46 @@ $(document).ready(function(){
   //Populate map result into html. If trails aren't returned, display error message and skip remaining steps.
 
   $("#trailSearch").on("click", function(){
-    var isValid;
-    //TODO: Add default radius value
-    var radius = $("#radius").val().trim() * 1.6; //Converts from miles to km
-    var locationInput = $("#locationInput").val().trim();
+    var isValid = true;
+    var radius = $("#radius").val().trim();
+    $("#radiusTooHighError").hide();
 
-    //Google Geocoding API
-    var geocodingData = getAjax("https://maps.googleapis.com" + "/maps/api/geocode/json" + "?address=" + locationInput + "&key=AIzaSyBOglb0LjVuwKiFDQz52dk_MSTfMJvbq5g");
-    isValid = geocodingDataIsValid(geocodingData);
+    if (radius == "") {
+      radius = 20;
+    }
+    else if (radius > 1000) {
+      $("#radiusTooHighError").show();
+      isValid = false;
+    }
+    
+    if(isValid){
+      var locationInput = $("#locationInput").val().trim();
+
+      //Google Geocoding API
+      var geocodingData = getAjax("https://maps.googleapis.com" + "/maps/api/geocode/json" + "?address=" + locationInput + "&key=AIzaSyBOglb0LjVuwKiFDQz52dk_MSTfMJvbq5g");
+      isValid = geocodingDataIsValid(geocodingData);
+    }
+    
 
     //Weather API
     if (isValid) {
-      var latitude = geocodingData.results[0].geometry.location.lat;
-      var longitude = geocodingData.results[0].geometry.location.lng;
-      var weatherData = getAjax("http://api.openweathermap.org" + "/data/2.5/weather" + "?lat=" + latitude + "&lon=" + longitude + "&appid=8f1e1fe8ad21fd905884311228d4b797");
+      searchLocation = { Latitude: geocodingData.results[0].geometry.location.lat, Longitude: geocodingData.results[0].geometry.location.lng };
+      var weatherData = getAjax("http://api.openweathermap.org" + "/data/2.5/weather" + "?lat=" + searchLocation.Latitude + "&lon=" + searchLocation.Longitude + "&appid=8f1e1fe8ad21fd905884311228d4b797");
       isValid = weatherData != false;
       //TODO: Populate weather data into html. Add error message logic if it isn't valid or results aren't returned.
     }
 
+    //FtPQ6JmTVtmshoiz8rYTQyhg7M7zp1A2x1NjsnfqQ6kJxvwTXr
     //Trails API
     if (isValid) {
-      var transitTrailsData = getAjax("https://api.transitandtrails.org" + "/api/v1/trailheads" + "?latitude=" + latitude + "&longitude=" + longitude+ "&key=d9881e8b3dd8b87a86a70c42a084d3f4029e9934d3657a6c4c0e71f91f7f79c0");
-      var trailsData = getAjax("https://mytrails.com.au" + "/service_location.php" + "?apikey=q8z7f7e2x9n1z8k7b4h2" + "&latitude=" + latitude + "&longitude=" + longitude + "&radius=" + radius);
-      //No 'Access-Control-Allow-Origin' header is present on the requested resource. Origin 'null' is therefore not allowed access.
-      //TODO: Find a damn trails api that works or figure out what's wrong with the api call!!!!
-      //TODO: Retrieve latitude and longitude for each trail (array) to use for next google api call
-      //TODO: Populate, check validity and reset isValid, add error message logic. Create new function???
+      var hikingTrailsApiData = getAjax("https://www.hikingproject.com" + "/data/get-trails" + "?lat=" + searchLocation.Latitude + "&lon=" + searchLocation.Longitude + "&maxDistance=" + radius + "&key=200147703-07bf8d789bb5f945f6246689684605c2");
+      
+      isValid = processTrailData(hikingTrailsApiData);
     }
 
     //Google Maps API
     if (isValid) {
+      getGoogleMapWithMarkers();
       //TODO: Api call to create google map with trail markers here (use latitude and longitude from above for markers).
       //May need to loop through array and make call for each trail's lat and lon. Or api may take all in one call.
       //TODO: Populate, check validity and reset isValid, add error message logic. Create new function???
@@ -88,4 +107,54 @@ $(document).ready(function(){
     }
   };
 
+  var processTrailData = function(trailData){
+    if(trailData.trails > 0){
+
+      for(var i in hikingTrailsApiData.trails) {
+          trailLocations.push({Latitude: hikingTrailsApiData.trails[i].latitude, Longitude: hikingTrailsApiData.trails[i].longitude});
+          populateTrailData(hikingTrailsApiData.trails[i]);
+      };
+      console.log(trailLocations);
+
+      //populateTrailData(hikingTrailsApiData.trails);
+
+    }
+    else{
+      return false;
+    }
+  }
+
+  var populateTrailData = function(trailInfo){
+
+  };
+
+
+
+  //Creating Google Map With Markers
+        var getGoogleMapWithMarkers = function initMap() {
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 3,
+          center: {lat: searchLocation.Latitude, lng: searchLocation.Longitude}
+        });
+
+        // Create an array of alphabetical characters used to label the markers.
+        var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        // Add some markers to the map.
+        // Note: The code uses the JavaScript Array.prototype.map() method to
+        // create an array of markers based on a given "locations" array.
+        // The map() method here has nothing to do with the Google Maps API.
+        var markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+          });
+        });
+
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }
+      var locations = trailLocations;
 });
