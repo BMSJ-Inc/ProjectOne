b1e11c16fd1b6b4f1bec4e491005550f5a62fd2f
Brooke Warren <brookewarren@Brocs-MacBook-Pro.local>
2017-09-14 01:13:45 -0400

    fixed click button isue

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,10 +2,11 @@ $(document).ready(function () {
 
     //Global variables
     var radius;
+    var locationInput;
 
     var geocodingDataIsValid = function (geocodingData) {
         if (geocodingData.results.length <= 0) {
-            $("#errorMessage").html("<strong>No results returned.</strong>");
+            $("#errorMessage").html("<strong>Enter a valid location.</strong>");
             $("#errorMessage").show();
             return false;
         }
@@ -26,28 +27,41 @@ $(document).ready(function () {
             url: "https://www.hikingproject.com" + "/data/get-trails" + "?lat=" + searchCoordinates.lat + "&lon=" + searchCoordinates.lng + "&maxDistance=" + radius + "&key=200147703-07bf8d789bb5f945f6246689684605c2",
             method: "GET",
             success: function (data) {
-                $("#trails-info").html("");
-                for (i = 0; i < data.trails.length; i++) {
-                    $("#trails-info").append("<h3>" + data.trails[i].name + "</h3>");
-                    $("#trails-info").append("<h4>" + data.trails[i].location + "</h4>");
-                    $("#trails-info").append("<p>" + data.trails[i].summary + "</p>");
-                    $("#trails-info").append("<strong>Difficulty Level: " + data.trails[i].difficulty + " | " + "Length: " + data.trails[i].length + " mi.</strong>");
-                    $("#trails-info").append("<h5>Rating: " + data.trails[i].stars + "/5</h5>");
-                    // Retrieving the URL for the image
-                    var imgURL = data.trails[i].imgSmall;
-                    // Creating an element to hold the image
-                    var image = $("<img>").attr("src", imgURL);
-                    // Appending the image
-                    $("#trails-info").append(image);
-        
-                    //TODO: Also append location info or more general trail data if possible           
-                    window.locations.push({ lat: data.trails[i].latitude, lng: data.trails[i].longitude });
+                if(data.trails.length <= 0) {
+                    $("#errorMessage").html("<strong>No trails found.</strong>");
+                    $("#errorMessage").show();
                 }
-                console.log(window.locations);
+                else {
+                    $("#trails-info").html("");
+                    for (i = 0; i < data.trails.length; i++) {
+                        $("#trails-info").append("<h3>" + data.trails[i].name + "</h3>");
+                        $("#trails-info").append("<h4>" + data.trails[i].location + "</h4>");
+                        $("#trails-info").append("<p>" + data.trails[i].summary + "</p>");
+                        $("#trails-info").append("<strong>Difficulty Level: " + data.trails[i].difficulty + " | " + "Length: " + data.trails[i].length + " mi.</strong>");
+                        $("#trails-info").append("<h5>Rating: " + data.trails[i].stars + "/5</h5>");
+                        // Retrieving the URL for the image
+                        var imgURL = data.trails[i].imgSmall;
+                        // Creating an element to hold the image
+                        var image = $("<img>").attr("src", imgURL);
+                        // Appending the image
+                        $("#trails-info").append(image);
+
+                        window.locations.push({ lat: data.trails[i].latitude, lng: data.trails[i].longitude });
+                    }
+                    console.log(window.locations);
+                    window.initMap();
+                }
                 console.log(data);
+            },
+            error: function (data) {
+                console.log(data);
+                $("#errorMessage").html("<strong>Unable to retrieve trails around " + locationInput + ".</strong>");
+                $("#errorMessage").show();
             },
             fail: function (data) {
                 console.log(data);
+                $("#errorMessage").html("<strong>An error occurred.</strong>");
+                $("#errorMessage").show();
             }
         });
     };
@@ -66,8 +80,15 @@ $(document).ready(function () {
                 $("#weatherIcon").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
                 getTrailsData();
             },
+            error: function (data) {
+                console.log(data);
+                $("#errorMessage").html("<strong>Unable to retrieve weather for " + locationInput + ".</strong>");
+                $("#errorMessage").show();
+            },
             fail: function (data) {
                 console.log(data);
+                $("#errorMessage").html("<strong>An error occurred.</strong>");
+                $("#errorMessage").show();
             }
         });
     };
@@ -80,34 +101,41 @@ $(document).ready(function () {
             success: function (data) {
                 console.log(data);
                 if (geocodingDataIsValid(data)) {
-                    window.searchCoordinates = { lat: data.results[0].geometry.location.lat, lng: data.results[0].geometry.location.lng}
+                    window.searchCoordinates = { lat: data.results[0].geometry.location.lat, lng: data.results[0].geometry.location.lng };
                     getWeatherData();
                 }
             },
+            error: function (data) {
+                console.log(data);
+                $("#errorMessage").html("<strong>Enter valid location.</strong>");
+                $("#errorMessage").show();
+            },
             fail: function (data) {
                 console.log(data);
+                $("#errorMessage").html("<strong>An error occurred.</strong>");
+                $("#errorMessage").show();
             }
         });
     };
 
     $("#trailSearch").on("click", function () {
-        var isValid = true;
+        var isRadiusValid = true;
         radius = $("#radius").val().trim();
-        $("#errorMessage").hide();
+        $("#errorMessage").hide(); //Clears previous error messages
+        window.locations = []; //Clears previous trail markers
 
         if (radius == "") {
             radius = 20;
         }
-        else if (radius > 1000) {
-            $("#errorMessage").html("<strong>Radius too large. Please search under 1000 miles.</strong>");
+        else if (radius > 1000 || radius < 0) {
+            $("#errorMessage").html("<strong>Radius must be between 0-1000 miles.</strong>");
             $("#errorMessage").show();
-            isValid = false;
+            isRadiusValid = false;
         }
 
-        if (isValid) {
-            var locationInput = $("#locationInput").val().trim();
-            getGeocodingData(locationInput);
-            window.initMap();
+        if (isRadiusValid) {
+            locationInput = $("#locationInput").val().trim();
+            getGeocodingData(locationInput);            
         };
     });
 });
